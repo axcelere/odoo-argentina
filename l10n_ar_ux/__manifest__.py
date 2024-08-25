4644bb42b556d85f09a9cfa6e207efa9d937a52c
Rafael Aguero Baquero <raguero@quanam.com>
2024-08-25 15:32:52 -0300

    [UPD] Submodules 17.0

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "17.0.1.2.1",
+    'version': "17.0.1.2.2",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
--- l10n_ar_ux/migrations/17.0.1.2.2/pre-migration.py
+++ l10n_ar_ux/migrations/17.0.1.2.2/pre-migration.py
@@ -9,6 +9,15 @@ def migrate(env, version):
     openupgrade.delete_records_safely_by_xml_id(
         env,
         [
+            'l10n_ar_ux.report_invoice_document',
+            'l10n_ar_edi.report_invoice_document',
+        ],
+        delete_childs=True
+    )
+    openupgrade.delete_records_safely_by_xml_id(
+        env,
+        [
+            'l10n_ar.report_invoice_document',
             'l10n_ar_ux.report_payment_group_document',
         ],
         delete_childs=True
