72e78eaa0ab2771e223d77b06e84e65394cc0d93
Rafael Aguero Baquero <raguero@quanam.com>
2024-12-28 03:19:35 -0300

    [ADD] Submodules v17

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "17.0.1.6.2",
+    'version': "17.0.1.6.3",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
