af646445aca256ae8f78904b41abf313d4e57275
Rafael Aguero Baquero <raguero@quanam.com>
2025-01-12 07:46:14 -0300

    [fix] VIEW INHERITANCE

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "17.0.1.6.7",
+    'version': "17.0.1.6.8",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
