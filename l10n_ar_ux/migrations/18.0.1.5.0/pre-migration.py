2aed78d15b2d510fc7898b62b7f77edfc0d9cde0
Rafael Aguero Baquero <raguero@quanam.com>
2025-04-27 14:12:13 -0300

    [MIG] l10n_ax_ux

--- l10n_ar_ux/migrations/18.0.1.5.0/pre-migration.py
+++ l10n_ar_ux/migrations/18.0.1.5.0/pre-migration.py
@@ -16,3 +16,22 @@ def migrate(cr, version):
     ''')
     for rec in env['account.payment.method'].search([]):
         rec.write({'code': '%s-%s' % (rec.code, 'old')})
+
+    # NO EXISTE MAS ESE TAG POR LO QUE DEBE SER LIMPIADO
+    tag_xml_id = 'l10n_ar_ux.tag_tax_jurisdiccion_924'
+    try:
+        tag = env.ref(tag_xml_id)
+    except ValueError:
+        tag = None
+
+    if tag:
+        # Buscar todas las repartition lines que referencian este tag
+        repartition_lines = env['account.tax.repartition.line'].search([
+            ('tag_ids', 'in', [tag.id])
+        ])
+
+        if repartition_lines:
+            for repartition in repartition_lines:
+                # Sacar el tag de las líneas (sin borrar la línea entera)
+                repartition.tag_ids = [(3, tag.id)]
+            env.cr.commit()  # importante si quieres asegurarte que se aplica antes del upgrade real
