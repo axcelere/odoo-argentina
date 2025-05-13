2f4b05c5ed75164fb308171f64f19db166f50ac9
Rafael Aguero Baquero <raguero@quanam.com>
2025-05-12 23:56:24 -0300

    [MIG] Limpiando old tags references

--- l10n_ar_ux/migrations/18.0.1.7.0/pre-migration.py
+++ l10n_ar_ux/migrations/18.0.1.7.0/pre-migration.py
@@ -104,3 +104,8 @@ def migrate(cr, version):
                 # Sacar el tag de las líneas (sin borrar la línea entera)
                 repartition.tag_ids = [(3, tag.id)]
             env.cr.commit()  # importante si quieres asegurarte que se aplica antes del upgrade real
+
+    cr.execute("""
+        DELETE FROM account_account_tag_account_move_line_rel
+        WHERE account_account_tag_id = 24
+    """)
