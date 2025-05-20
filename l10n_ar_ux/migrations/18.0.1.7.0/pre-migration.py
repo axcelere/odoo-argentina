ab6e8b006da93ed50f5cc9ac55554bb1ce1af09a
Rafael Aguero Baquero <raguero@quanam.com>
2025-05-19 23:31:55 -0300

    [MIG] l10n_ar_ux

--- l10n_ar_ux/migrations/18.0.1.7.0/pre-migration.py
+++ l10n_ar_ux/migrations/18.0.1.7.0/pre-migration.py
@@ -145,6 +145,6 @@ def migrate(cr, version):
     """)
     cr.execute("""
         DELETE FROM account_account_tag_account_tax_repartition_line_rel
-        WHERE account_account_tag_id IN (24,41,40,38,37,36,35)
+        WHERE account_account_tag_id IN (24,41,40,38,37,36,35,34,33,32,31,30)
     """)
     env.cr.commit()
