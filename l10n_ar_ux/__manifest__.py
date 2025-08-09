0b6d13abb192ed3642d0cc6306691a1ec174189a
Rafael Aguero Baquero <raguero@quanam.com>
2025-08-08 22:50:27 -0300

    [MIG]

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Argentinian Accounting UX",
-    "version": "18.0.1.7.3",
+    "version": "18.0.1.7.4",
     "category": "Localization/Argentina",
     "sequence": 14,
     "author": "ADHOC SA",
