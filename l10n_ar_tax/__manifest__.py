a38f4d9145e1e2aaa28ca1077ae8130c3efeecda
Rafael Aguero Baquero <raguero@quanam.com>
2025-09-27 20:09:46 -0300

    [MIG]

--- l10n_ar_tax/__manifest__.py
+++ l10n_ar_tax/__manifest__.py
@@ -32,7 +32,7 @@
         "views/res_partner_view.xml",
         "views/account_tax_view.xml",
         "views/account_move_views.xml",
-        "views/report_payment_receipt_templates.xml",
+        # TODO UPGRADE "views/report_payment_receipt_templates.xml",
         "views/l10n_ar_payment_withholding_views.xml",
         "views/account_fiscal_position_view.xml",
         # "wizard/account_payment_register_views.xml",
